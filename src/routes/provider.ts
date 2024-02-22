ac689ac9ccb5ceeccc7854b49a2e53aecfb08f2d
Jhoan <alvaradojhan690@gmail.com>
2024-02-21 21:25:41 -0500

    Update structure main

--- src/routes/provider.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import mongoose from "mongoose";
-import { idProvider } from "../controllers/provider-controller";
-
-const router = Router();
-
-router.post('/getproviders', idProvider)
-
-export default router;
