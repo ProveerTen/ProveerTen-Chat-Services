923c2ce46889ba317aa496753440d5bc280419cb
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-27 15:33:01 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,24 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import morgan from 'morgan';
-import { Server as SocketIOServer } from 'socket.io'
+import { Server as SocketIOServer } from 'socket.io';
 import provider from './routes/provider';
-import chat from './routes/chat'
+import chat from './routes/chat';
 import socketController from './controllers/socket-controller';
 
 class Server {
-
     private app: express.Application;
     private port: string;
-    private port_chat: string;
     private httpServer: http.Server;
     private io: SocketIOServer;
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '3000';
-        this.port_chat = process.env.PORT_CHAT || '10101';
         this.httpServer = http.createServer(this.app);
-        const allowedOrigins = ['https://proveerten.netlify.app','https://proveer-ten-front-end-git-hu-featurechat-bryctans-projects.vercel.app','http://localhost:4200'];
+        const allowedOrigins = ['https://proveerten.netlify.app', 'https://proveer-ten-front-end-git-hu-featurechat-bryctans-projects.vercel.app', 'http://localhost:4200'];
         this.io = new SocketIOServer(this.httpServer, {
-            cors: { origin: 'allowedOrigins' }
+            cors: { origin: allowedOrigins }
         });
         this.middlewares();
         this.routes();
@@ -30,18 +27,14 @@ class Server {
     }
 
     listen() {
-        this.httpServer.listen(this.port_chat, () => {
-            console.log(`Server chat running on port ${this.port_chat}`);
-        });
-
-        this.app.listen(this.port, () => {
+        this.httpServer.listen(this.port, () => {
             console.log(`Server running on port ${this.port}`);
         });
     }
 
     middlewares() {
         this.app.use(express.json());
-        const allowedOrigins = ['https://proveerten.netlify.app','https://proveer-ten-front-end-git-hu-featurechat-bryctans-projects.vercel.app','http://localhost:4200'];
+        const allowedOrigins = ['https://proveerten.netlify.app', 'https://proveer-ten-front-end-git-hu-featurechat-bryctans-projects.vercel.app', 'http://localhost:4200'];
         this.app.use(cors({ credentials: true, origin: allowedOrigins }));
         this.app.use(morgan('dev'));
     }
@@ -52,7 +45,8 @@ class Server {
         });
 
         this.app.use('/provider', provider);
-        this.app.use('/chat', chat)
+        this.app.use('/chat', chat);
     }
 }
-export default Server
+
+export default Server;
