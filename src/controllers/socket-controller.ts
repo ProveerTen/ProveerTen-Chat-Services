599dd060ae643b9182ae7ad2ccf6a29baf6578ab
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-27 17:41:30 -0500

    Update socket-controller.ts

--- src/controllers/socket-controller.ts
+++ src/controllers/socket-controller.ts
@@ -21,13 +21,13 @@ export default function socketController(io: SocketIOServer) {
         socket.on('message', async (message: any, roomId: string) => {
 
             console.log('Message received:', message);
-
+            /*
             // const prompt = `En el lenguaje y dialecto colombiano y de la region del caribe colombiano, evalúa la palabra "${message.mensaje}" por su carácter ofensivo o grosero. Responde con "SÍ" si es una grosería o una ofensa, o "NO" si no lo es.`;
             const prompt = `En el lenguaje y dialecto colombiano y de la regiones de Colombia "${message.mensaje}" por su carácter ofensivo o grosero. Responde con "SÍ" si es una grosería o una ofensa, o "NO" si no lo es.`;
             
             // const prompt = `En el lenguaje colombiano, considera la palabra "${message.mensaje}" utilizada de forma potencialmente ofensiva. ¿Es una grosería o una ofensa? Responde con "SI" o "NO".`
-
-
+    */
+    /*
                 try {
                     
                   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
@@ -35,10 +35,13 @@ export default function socketController(io: SocketIOServer) {
                   const response = await result.response;
                   const text = response.text();
                   console.log (text)
+                  
                   if (text === "SÍ") {  
+                  *
                     message.mensaje = "Este mensaje no cumple las politicas establecidas"
                     io.to(roomId).emit('message', message);
                   } else if (text === "NO") {
+                  */
                     const newMessage = new models.Message({
                       sender: message.user,
                       content: message.mensaje,
@@ -48,17 +51,17 @@ export default function socketController(io: SocketIOServer) {
                     if (chat) {
                       chat.messages.push(newMessage);
                       await chat.save();
-                      io.to(roomId).emit(`message:${roomId}`, newMessage); 
-                      console.log("No es grosería");
+                      io.to(roomId).emit(`message:${roomId}`, newMessage);
                     } else {
                       console.log("No se encontró la sala de chat con ID:", roomId);
                     }
+            /*
                   } else {
                   console.log("No se pudo determinar si hay grocerias en el mensaje");
                 }
                 } catch (error) {
                   console.error("Error de la IA:", error);
-                }
+                } */
         });
 
         socket.on('disconnect', () => {
