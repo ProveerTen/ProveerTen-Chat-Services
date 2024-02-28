3322692d0d35d6ceab0549c306e8432be35208e7
Bryctan <juanchoquintero1004@gmail.com>
2024-02-28 15:51:09 -0500

    implementation get chats

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -29,3 +29,21 @@ export const messages = async (req: Request, res: Response) => {
   res.status(200).json({ chat:chat[0] });
 }
 
+export const getchats = async (req: Request, res: Response) => {
+  try {
+    let role:string = req.body.role;
+    let id:string = req.body.id;
+    let field:any = `participants.${role}Id`;
+
+    let search:any = {};
+    search[field] = id;
+
+    console.log(search);
+    let values:any = role === 'grocer' ? {_id: 1,'participants.providerId': 1 } : {_id: 1, 'participants.grocerId': 1} ;
+    let chats = await model.Chat.find(search).select(values);
+
+    res.status(200).json({ chats });
+  } catch (error:any) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createChat, findChat, messages } from "../controllers/chat-controller";
+import { createChat, findChat, getchats, messages } from "../controllers/chat-controller";
 
 const router = Router();
  
 router.post('/create', createChat);
 router.post('/find', findChat);
 router.post('/getmessages', messages);
+router.post('/getchats', getchats);
 export default router;
